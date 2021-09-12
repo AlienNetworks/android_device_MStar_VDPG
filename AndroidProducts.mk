72ac7e4b636b8a92bcc0b84017970a80a34967e1
Allen <AlienNetworks@users.noreply.github.com>
2021-09-12 20:14:09 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,5 +15,6 @@
 #
 
 #LOCAL_PATH := device/MStar/sugarcane
-
-PRODUCT_MAKEFILES := device/MStar/sugarcane/omni_sugarcane.mk
+#PRODUCT_MAKEFILES := device/MStar/sugarcane/omni_sugarcane.mk
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_sugarcane.mk
