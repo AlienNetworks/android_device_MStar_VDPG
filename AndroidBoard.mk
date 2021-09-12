4470a2d68abbc8a438812134128ba153d9726e9e
Allen <AlienNetworks@users.noreply.github.com>
2021-09-12 13:19:09 +0100

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -23,8 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-LOCAL_PATH := $(call my-dir)
-
 ifneq ($(filter sugarcane,$(TARGET_DEVICE)),)
+LOCAL_PATH := device/Mstar/sugarcane
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
