87e9f59b0305339dd5703979eaef5591e4d8af65
Allen <AlienNetworks@users.noreply.github.com>
2021-09-16 20:38:23 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -32,8 +32,3 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 
 endif
-
-##ifneq ($(filter sugarcane,$(TARGET_DEVICE)),)
-#LOCAL_PATH := device/MStar/sugarcane
-#include $(call all-makefiles-under,$(LOCAL_PATH))
-#endif
